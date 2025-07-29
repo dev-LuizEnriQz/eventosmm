1142614004652911da6da8f76771d29ae0202c27
Luiz Enriquez <luizenriquez@Luizs-MacBook-Pro.local>
2025-07-28 20:31:57 -0600

    Se actualiza el navbar Admin con estilo hover

--- resources/views/layouts/navigationAdmin.blade.php
+++ resources/views/layouts/navigationAdmin.blade.php
@@ -6,35 +6,29 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                     <!--CLIENTES-->
-                    <li class="nav-item nav-underline">
-                        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#clientsMenu" aria-expanded="false">Clientes</a>
-                        <div class="collapse" id="clientsMenu">
-                            <ul class="dropdown-menu" id="clientsMenu">
-                                <li><a class="dropdown-item" href="{{route('clients.index')}}">Consulta Clientes</a></li>
-                                <li><a class="dropdown-item" href="{{ route('clients.registrarCliente') }}">Registrar Cliente</a></li>
-                            </ul>
-                        </div>
+                    <li class="nav-item dropdown nav-underline">
+                        <a class="nav-link" href="#" id="clientsDropdown" role="button">Clientes</a>
+                        <ul class="dropdown-menu" aria-labelledby="clientsDropdown">
+                            <li><a class="dropdown-item" href="{{route('clients.index')}}">Consulta Clientes</a></li>
+                            <li><a class="dropdown-item" href="{{ route('clients.registrarCliente') }}">Registrar Cliente</a></li>
+                        </ul>
                     </li>
 
                     <!-- COTIZACIONES-->
-                    <li class="nav-item nav-underline">
-                        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#quoteMenu" aria-expanded="false">Cotizaciones</a>
-                        <div class="collapse" id="quoteMenu">
-                            <ul class="dropdown-menu" id="quoteMenu">
+                    <li class="nav-item dropdown nav-underline">
+                        <a class="nav-link" href="#" id="quoteMenu" role="button">Cotizaciones</a>
+                            <ul class="dropdown-menu" aria-labelledby="quoteMenu">
                                 <li><a class="dropdown-item" href="{{route('quotes.index')}}">Consultar Cotización</a></li>
                                 <li><a class="dropdown-item" href="{{route('quotes.registrarCotizacion')}}">Registrar Cotización</a></li>
                             </ul>
-                        </div>
                     </li>
 
                     <!--DEPOSITOS-->
-                    <li class="nav-item nav-underline">
-                        <a class="nav-link" href="#" data-bs-toggle="collapse" data-bs-target="#depositsMenu" aria-expanded="false">Depositos</a>
-                        <div class="collapse" id="depositsMenu">
-                            <ul class="dropdown-menu" id="depositsMenu">
+                    <li class="nav-item dropdown nav-underline">
+                        <a class="nav-link" href="#" id="#depositsMenu" role="button">Depositos</a>
+                            <ul class="dropdown-menu" aria-labelledby="depositsMenu">
                                 <li><a class="dropdown-item" href="{{route('deposits.accounts.index')}}">Consultar Cuenta / Depositos</a></li>
                             </ul>
-                        </div>
                     </li>
 
                     <!--CALENDARIO-->
@@ -49,3 +43,9 @@
         </div>
     </nav>
 
+<style>
+    .nav-item.dropdown:hover .dropdown-menu {
+        display: block;
+        margin-top: 0;
+    }
+</style>
